1b7cc45ca21bf445b572ed4a1009a45b7bb500c0
Gil Miranda <gilsmneto@gmail.com>
2019-04-07 17:24:58 -0300

    Começando a primeira aplicação do curso

---
+++ udemy-python-mega-course/app1-dictionary/app1.py
@@ -0,0 +1,14 @@
+import json
+
+data = json.load(open('data/data.json'))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    else:
+        return 'The word doesn\'t exist, please double check it.'
+
+word = input('Enter word: ')
+
+print(translate(word))
