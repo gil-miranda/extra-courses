5b2c7482e2475e04a666bf43f915fd25fa326be7
Gil <gilsmneto@gmail.com>
2019-08-12 06:50:07 -0300

    udemy

---
+++ udemy-cpp/cmake-build-debug/CMakeFiles/udemy_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/udemy_cpp.dir/main.cpp.o"
+  "udemy_cpp.pdb"
+  "udemy_cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/udemy_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
